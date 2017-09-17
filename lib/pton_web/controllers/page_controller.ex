9b2a50bfd9e93d6e5b49bfbf29ba53bfc5035183
Casey Chow <casey.chow@gmail.com>
2017-09-17 11:28:59 -0400

    index form

--- lib/pton_web/controllers/page_controller.ex
+++ lib/pton_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule PtonWeb.PageController do
   use PtonWeb, :controller
 
+  alias Pton.Redirection
+  alias Pton.Redirection.Link
+
   def index(conn, _params) do
-    render conn, "index.html"
+    changeset = Redirection.change_link(%Link{})
+    render(conn, "index.html", changeset: changeset)
   end
 end
